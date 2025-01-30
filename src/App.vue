3fe24339cbb6afec139d4f7c88a8a84da3ef5cd2
Fabio Gaspari <fabio.gaspari@gmail.com>
2025-01-30 10:14:12 -0300

    initial commit and project configurations

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="w-screen bg-blue-500 p-4 top-0 left-0">
+        Esta div está no topo da página.
+    </div>
+</template>
+
+<style scoped></style>
