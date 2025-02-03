01da23672e0e9aac9385c613f28b3e767a276ee2
Fabio Gaspari <fabio.gaspari@gmail.com>
2025-02-03 05:10:13 -0300

    ajustes ao editar a tarefa - data incorreta

--- src/components/base/HeadQuindim.vue
+++ src/components/base/HeadQuindim.vue
@@ -1,10 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import router from "@/router";
+import { useAuthStore } from "@/stores/authStore";
 
+const authStore = useAuthStore()
+
+</script>
 <template>
 
     <div class="w-screen bg-white p-4 top-0 left-0">
-        <div class="flex justify-center">
+        <div class="flex justify-between" v-if="authStore.isAuthenticated">
             <img src="@/assets/logo_quindim.png" alt="Logo Quindim" id="logomarca" />
+            <q-btn color="primary" label="Menu" style="background: #28606d;">
+                <q-menu>
+                    <q-list style="min-width: 100px">
+                        <q-item clickable v-close-popup>
+                            <q-item-section @click="router.push({ name: 'tasks.list' })">Tarefas</q-item-section>
+                        </q-item>
+                        <q-item clickable v-close-popup>
+                            <q-item-section @click="router.push({ name: 'task-status.list' })">Status de
+                                Tarefa</q-item-section>
+                        </q-item>
+                    </q-list>
+                </q-menu>
+            </q-btn>
+        </div>
+        <div class="flex justify-between" v-else>
+            <div class="flex justify-center" v-if="authStore.isAuthenticated">
+                <img src="@/assets/logo_quindim.png" alt="Logo Quindim" id="logomarca" />
+            </div>
         </div>
     </div>
 </template>
--- src/utils/DateUtil.ts
+++ src/utils/DateUtil.ts
@@ -1,9 +1,10 @@
 export default class DateUtil {
-  static formatDate(dataString: string) {
-    const data = new Date(dataString)
-    const dia = data.getDate().toString().padStart(2, '0')
-    const mes = (data.getMonth() + 1).toString().padStart(2, '0')
-    const ano = data.getFullYear()
+  static formatDate(dateString: string) {
+    const date = new Date(dateString)
+    date.setDate(date.getDate() + 1)
+    const dia = date.getDate().toString().padStart(2, '0')
+    const mes = (date.getMonth() + 1).toString().padStart(2, '0')
+    const ano = date.getFullYear()
 
     return `${dia}/${mes}/${ano}`
   }
